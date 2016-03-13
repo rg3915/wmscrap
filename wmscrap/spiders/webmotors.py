d8989ad69ecad399edac2194da42decdbd3c0dba
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-03-13 18:52:38 -0300

    Parse car link to get links of cars

--- wmscrap/spiders/webmotors.py
+++ wmscrap/spiders/webmotors.py
@@ -5,9 +5,47 @@ import scrapy
 class WebmotorsSpider(scrapy.Spider):
     name = "webmotors"
     allowed_domains = ["webmotors.com.br"]
-    start_urls = (
-        'http://www.webmotors.com.br/',
+    first_page_url = (
+        'http://www.webmotors.com.br/comprar/carros/novos-usados/'
+        'veiculos-todos-estados/?tipoveiculo=carros'
+        '&tipoanuncio=novos%7Cusados'
+        '&estado1=veiculos-todos-estados&o=1&p={}'
     )
 
-    def parse(self, response):
-        pass
+    def __init__(self):
+        self.last_page = None
+
+    def start_requests(self):
+        request = scrapy.Request(
+            url=WebmotorsSpider.first_page_url.format(1),
+            callback=self.parse_page,
+        )
+        yield request
+
+    def parse_page(self, response):
+        # get anchor link on "last page" button
+        last_button_anchor = response.xpath(
+            '//*[contains(@id, "boxResultado")]'
+            '/div/a/@href')[-1].extract()
+
+        # get the last page from href
+        self.last_page = int(last_button_anchor.split("p=")[-1])
+
+        for page in range(1, self.last_page):
+            request = scrapy.Request(
+                url=WebmotorsSpider.first_page_url.format(page),
+                callback=self.parse_car_link,
+            )
+            yield request
+
+    def parse_car_link(self, response):
+        results = response.xpath(
+            '//*[contains(@id, "boxResultado")]'
+            '/a/@href')
+
+        for number, car in enumerate(results, 1):
+            print("{}. {}".format(number, car.extract()))
+
+        print("-----")
+        print(response.url)
+        print("-----")
